7f67e7e89dec0e038873f1af0994c637715208fb
Kamikazexxxl <kamikazexxxl@gmail.com>
2022-04-27 15:55:56 +0300

    Подключение к БД Создание юзеров

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,17 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм
+        Util util = new Util();
+        User user1 = new User("Ivan", "Ivanov", (byte) 20);
+        User user2 = new User("Petr", "Petrov", (byte) 25);
+        User user3 = new User("Magomed", "Magomedov", (byte) 30);
+        User user4 = new User("Joe", "Johnson", (byte) 40);
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private String sqlUrl = "jdbc:mysql://localhost:3306/kataschema";
+    private String sqlUser = "root";
+    private String sqlPassword = "12345zZ!";
+
+    {
+        try (Connection connection = DriverManager.getConnection(sqlUrl, sqlUser, sqlPassword)) {
+            System.out.println("Соединение с MySQL успешно");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
