03d5335e7ee54e44d9c5d056c04ef69bfc4b1ccf
Kamikazexxxl <kamikazexxxl@gmail.com>
2022-05-09 18:35:15 +0300

    Criteria не надо использовать, сделай без неё, и добавь rollback-и

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.query.Query;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,73 +30,101 @@ public class UserDaoHibernateImpl implements UserDao {
 
         Transaction transaction = session.beginTransaction();
 
-        Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
-                        "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-                        "name VARCHAR(50) NOT NULL, " +
-                        "lastName VARCHAR(50) NOT NULL, " +
-                        "age TINYINT NOT NULL)").
-                addEntity(User.class);
-        query.executeUpdate();
-        transaction.commit();
-//        session.close();
+        try {
+            Query query = session.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+                            "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                            "name VARCHAR(50) NOT NULL, " +
+                            "lastName VARCHAR(50) NOT NULL, " +
+                            "age TINYINT NOT NULL)").
+                    addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
 
         Transaction transaction = session.beginTransaction();
+        try {
 
-        Query query = session.createSQLQuery("DROP TABLE IF EXISTS users").
-                addEntity(User.class);
-        query.executeUpdate();
-        transaction.commit();
-//        session.close();
+
+            Query query = session.createSQLQuery("DROP TABLE IF EXISTS users").
+                    addEntity(User.class);
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        session.beginTransaction();
-        session.save(new User(name, lastName, age));
-        session.getTransaction().commit();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+            System.out.printf("User с именем - %s добавлен в базу данных\n", name);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
+
+
 
 
     @Override
     public void removeUserById(long id) {
         Transaction transaction = null;
-
+        try (Session session = Util.getSessionFactory().openSession()) {
         transaction = session.beginTransaction();
         User user = session.get(User.class, id);
         if (user != null) {
             session.remove(user);
         }
         transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
 
     @Override
     public List<User> getAllUsers() {
-
-        session.beginTransaction();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<User> criteria = builder.createQuery(User.class);
-        criteria.from(User.class);
-        List<User> entityList = session.createQuery(criteria).getResultList();
-        for (User e : entityList) {
-            if (e == null) {
-                System.out.println("NNNUUULLL");
-            }
-            System.out.println(e);
+        try (Session session = Util.getSessionFactory().openSession()) {
+            return session.createQuery("select a from User a", User.class).getResultList();
         }
-        session.getTransaction().commit();
-        return entityList;
     }
 
 
     @Override
     public void cleanUsersTable() {
-        session.beginTransaction();
-        session.createQuery("delete User").executeUpdate();
-        session.getTransaction().commit();
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
